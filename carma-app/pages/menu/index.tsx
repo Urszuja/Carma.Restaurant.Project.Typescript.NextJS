3d0b461012a40b91ad3284554ac6e87f0eead45a
Urszula Jarych <urszula.jarych@gmail.com>
2022-12-09 14:58:24 +0100

    header created, brand images added, new meta added

--- carma-app/pages/menu/index.tsx
+++ carma-app/pages/menu/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
+import Header from '../../components/Header'
 
 function MenuPage() {
   return (
     <div>
+      <Header/>
         <h4>Menu Page</h4>
         <div>
             <p>Go to:</p>
