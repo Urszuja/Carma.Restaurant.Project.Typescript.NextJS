322f6f80e655e07c9fe69df8ad16da5856375c84
Urszula Jarych <urszula.jarych@gmail.com>
2022-12-19 15:03:17 +0100

    addOrderItem and removeOrderItem methods added

--- carma-app/model/Order.ts
+++ carma-app/model/Order.ts
@@ -10,6 +10,10 @@ interface IOrder {
     orderItems: OrderItem[];
     totalPrice: number;
     status: Status;
+
+    addOrderItem: (orderItem: OrderItem) => void ;
+
+    removeOrderItem: ( id: string) => void;
 }
 
 export default class Order implements IOrder {
@@ -18,5 +22,18 @@ export default class Order implements IOrder {
     constructor(public client: Client, public orderItems: OrderItem[], public status: Status){
         this.id = uuidv4(); 
         this.totalPrice = 0;
+    }
+
+    addOrderItem(orderItem: OrderItem): void {
+
+        this.orderItems.map(item => 
+            
+        (item.name === orderItem.name && item.size === orderItem.size) ? item.quantity += 1 : this.orderItems.push(item)) 
+        ;
+    }
+
+    removeOrderItem (id: string): void {
+       
+        this.orderItems.filter(item => item.id === id);
     }
 }
