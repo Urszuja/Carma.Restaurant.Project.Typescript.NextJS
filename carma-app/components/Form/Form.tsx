6df79fd447bc8a06d4cccc6fd9827d462943ba35
Urszula Jarych <urszula.jarych@gmail.com>
2022-12-21 20:47:52 +0100

    react hooks form added

--- carma-app/components/Form/Form.tsx
+++ carma-app/components/Form/Form.tsx
@@ -1,38 +1,74 @@
 import React from "react";
 import StyledForm from "./Form.styled";
 import type Client from "../../model/Client";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+//to be replaced by client
+// order for client comes from
+type Inputs = {
+  firstName: string;
+  lastName: string;
+  phoneNumber: number;
+};
 
 function Form() {
-  const handleSubmit = (e: React.SyntheticEvent<EventTarget>) => {
-    console.log(e.target);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    //POST CLIENT DATA WITH ORDER
+    window.localStorage.clear();
   };
 
   return (
-    <StyledForm action="#" method="post">
-      <label htmlFor="first">First name:</label>
+    <StyledForm onSubmit={handleSubmit(onSubmit)}>
+      {/* First name input*/}
+      <label>Your first name:</label>
+      {errors.firstName && <span>Name is required</span>}
       <input
-        type="text"
-        id="first"
-        name="first"
-        min={2}
-        max={30}
-        required
-        pattern="[A-Za-z]"
-        autoFocus
-        placeholder="first name"
+        {...register("firstName", {
+          required: true,
+          pattern: /^[A-Za-z]+$/i,
+          minLength: 2,
+          maxLength: 20,
+        })}
       />
-      <label htmlFor="last">Last name:</label>
+
+      {/* Last name input */}
+      <label>Your last name:</label>
+      {errors.lastName && <span>Last name is required</span>}
       <input
-        type="text"
-        id="last"
-        name="last"
-        required
-        min={2}
-        max={50}
-        pattern="[A-Za-z]"
-        placeholder="last name"
+        {...register("lastName", {
+          required: true,
+          pattern: /^[A-Za-z]+$/i,
+          minLength: 2,
+          maxLength: 50,
+        })}
       />
-      <button type="submit">Submit</button>
+      {/* Phone number input */}
+      {/* <label>Your phone:</label>
+      {errors.phoneNumber && <span>Phone number is required</span>}
+      <input
+        defaultValue="phone number"
+        {...register("phoneNumber", {
+          required: true,
+          pattern: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+        })}
+      /> */}
+
+      {/* Submit button  */}
+
+      <input id="submitButton" type="submit" />
     </StyledForm>
   );
 }
