68a9a7d6f07161c3476815b7eb67ca097b3204d4
Urszula Jarych <urszula.jarych@gmail.com>
2022-12-29 10:43:15 +0100

    order list created

---
+++ carma-app/components/OrderList/OrderList.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { testOrder } from "../../mockData";
+
+function OrderList() {
+  return <div>testOrder.map()</div>;
+}
+
+export default OrderList;
--- carma-app/mockData.ts
+++ carma-app/mockData.ts
@@ -1,10 +1,22 @@
-``
+``;
 import Client, { Address } from "./model/Client";
 import Order from "./model/Order";
 import OrderItem from "./model/OrderItem";
 
-export const testOrderItem = new OrderItem('margharita',2,10,2)
-export const testAddress : Address = ['Jandy', 21, 37, 12345, 'Biala Podlaska'];
-export const testClient = new Client('Krystyna','Janda','krystyna@janda.pl',1011700, [], testAddress);
-export const testOrder = new Order(testClient,[testOrderItem],'pending');
-export const mockClientsDataBase  = [testClient];
+export const testOrderItem = new OrderItem("margharita", 2, 10, 2);
+export const testOrderItem2 = new OrderItem("salame", 1, 15, 1);
+export const testAddress: Address = ["Jandy", 21, 37, 12345, "Biala Podlaska"];
+export const testClient = new Client(
+  "Krystyna",
+  "Janda",
+  "krystyna@janda.pl",
+  1011700,
+  [],
+  testAddress
+);
+export const testOrder = new Order(
+  testClient,
+  [testOrderItem, testOrderItem2],
+  "pending"
+);
+export const mockClientsDataBase = [testClient];
