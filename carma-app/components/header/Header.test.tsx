6b5f55d8fe89ffd7840db9a43a6ca75809d8d5f8
Urszula Jarych <urszula.jarych@gmail.com>
2023-01-24 20:23:16 +0100

    tests for Header added

---
+++ carma-app/components/header/Header.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Header from "./Header";
+import {
+  DataStoreContext,
+  DataStoreContextInterface,
+} from "../DataStoreContext";
+import createMockStore from "../../mocks/MockStore";
+describe("testing header functionality ", () => {
+  const mockStore = createMockStore();
+  test("header is rendering correctly", () => {
+    render(
+      <DataStoreContext.Provider value={mockStore}>
+        <Header />
+      </DataStoreContext.Provider>
+    );
+
+    const restaurantName = screen.getByText(/Carma/i);
+    const cartItems = document.querySelector(".order-items")! as HTMLDivElement;
+    const cartItemsNumber = parseInt(cartItems.innerHTML);
+
+    expect(restaurantName).toBeInTheDocument();
+    expect(cartItems).toBeInTheDocument();
+    expect(cartItemsNumber).toBe(2);
+  });
+});
