12ccc2ba020d92e011bc10192ab7198b7d429aaf
Urszula Jarych <urszula.jarych@gmail.com>
2023-01-25 10:43:44 +0100

    failing test for add to basket

--- carma-app/components/AddToBasketItem/AddToBasketItem.test.tsx
+++ carma-app/components/AddToBasketItem/AddToBasketItem.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { act, render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import AddToBasketItem from "./AddToBasketItem";
 import { testMenuItem } from "../../mocks/mockData";
@@ -8,14 +8,16 @@ import {
 } from "../DataStoreContext";
 import createMockStore from "../../mocks/MockStore";
 
-import * as uuid from "uuid";
+//import * as uuid from "uuid";
 import OrderItemInstance from "../../model/OrderItem";
+
 jest.mock("uuid", () => {
   const originalModule = jest.requireActual("uuid");
   return { ...originalModule, v4: () => "123456789" };
 });
 
 describe("test adding to basket", () => {
+  const addToBasket = jest.fn();
   test("add to basket renders properly", () => {
     render(
       <AddToBasketItem
@@ -26,14 +28,71 @@ describe("test adding to basket", () => {
         description={testMenuItem.description}
         isSpicy={testMenuItem.isSpicy}
         isVegan={testMenuItem.isVegan}
-        closeBasket={() => {
-          console.log("basket closed");
-        }}
+        closeBasket={addToBasket}
       />
     );
     const name = screen.getByText(/Margharita/i);
 
     expect(name).toBeInTheDocument();
+  });
+
+  test("increase amount", () => {
+    render(
+      <AddToBasketItem
+        name={testMenuItem.name}
+        id={testMenuItem.id}
+        prices={testMenuItem.prices}
+        image={testMenuItem.image}
+        description={testMenuItem.description}
+        isSpicy={testMenuItem.isSpicy}
+        isVegan={testMenuItem.isVegan}
+        closeBasket={addToBasket}
+      />
+    );
+
+    const increaseButton = document.querySelector(
+      '[alt="plus"]'
+    )! as HTMLImageElement;
+
+    act(() => increaseButton.click());
+
+    const quantityDiv = document.querySelector(".number")! as HTMLDivElement;
+    const quantityNum = parseInt(quantityDiv.innerHTML);
+    expect(increaseButton).toBeInTheDocument();
+    expect(quantityNum).toBe(2);
+  });
+
+  test("decrease amount", () => {
+    render(
+      <AddToBasketItem
+        name={testMenuItem.name}
+        id={testMenuItem.id}
+        prices={testMenuItem.prices}
+        image={testMenuItem.image}
+        description={testMenuItem.description}
+        isSpicy={testMenuItem.isSpicy}
+        isVegan={testMenuItem.isVegan}
+        closeBasket={addToBasket}
+      />
+    );
+
+    const decreaseButton = document.querySelector(
+      '[alt="minus"]'
+    )! as HTMLImageElement;
+    const increaseButton = document.querySelector(
+      '[alt="plus"]'
+    )! as HTMLImageElement;
+
+    act(() => {
+      increaseButton.click();
+      increaseButton.click();
+      decreaseButton.click();
+    });
+    const quantityDiv = document.querySelector(".number")! as HTMLDivElement;
+    const quantityNum = parseInt(quantityDiv.innerHTML);
+
+    expect(decreaseButton).toBeInTheDocument();
+    expect(quantityNum).toBe(2);
   });
 
   test("adding new pizza (different type and size) to basket", async () => {
@@ -50,16 +109,14 @@ describe("test adding to basket", () => {
           description={testMenuItem.description}
           isSpicy={testMenuItem.isSpicy}
           isVegan={testMenuItem.isVegan}
-          closeBasket={() => {
-            console.log("basket closed");
-          }}
+          closeBasket={addToBasket}
         />
       </DataStoreContext.Provider>
     );
-    const pizzaLarge = document.querySelector(
+    const pizzaMedium = document.querySelector(
       "input#medium"
     )! as HTMLInputElement;
-    pizzaLarge.click();
+    pizzaMedium.click();
     const submitButton = document.querySelector("button")!;
     submitButton.click();
     await waitFor(() => {
@@ -82,6 +139,52 @@ describe("test adding to basket", () => {
           id: "123456789",
           name: "Margharita",
           price: 10,
+          quantity: 1,
+          size: "M",
+        } as OrderItemInstance,
+      ]);
+    });
+  });
+
+  test("adding one more pizza of same size and type to basket", async () => {
+    const store = createMockStore() as DataStoreContextInterface;
+    const mockedSetState = jest.fn();
+
+    render(
+      <DataStoreContext.Provider value={{ ...store, setCart: mockedSetState }}>
+        <AddToBasketItem
+          name={testMenuItem.name}
+          id={testMenuItem.id}
+          prices={testMenuItem.prices}
+          image={testMenuItem.image}
+          description={testMenuItem.description}
+          isSpicy={testMenuItem.isSpicy}
+          isVegan={testMenuItem.isVegan}
+          closeBasket={addToBasket}
+        />
+      </DataStoreContext.Provider>
+    );
+    const pizzaSmall = document.querySelector(
+      "input#small"
+    )! as HTMLInputElement;
+    const submitButton = document.querySelector("button")!;
+    act(() => {
+      pizzaSmall.click();
+      submitButton.click();
+    });
+    await waitFor(() => {
+      expect(mockedSetState).toHaveBeenCalledWith([
+        {
+          id: "123456789",
+          name: "Margharita",
+          price: 10,
+          quantity: 3,
+          size: "S",
+        } as OrderItemInstance,
+        {
+          id: "123456789",
+          name: "Salame",
+          price: 15,
           quantity: 1,
           size: "M",
         } as OrderItemInstance,
